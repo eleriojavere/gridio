4a35eac21b557f2576b79a20b816619f530c7a41
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-10-29 13:22:48 +0300

    Render clean electricity data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import "./App.scss";
+
+interface Data {
+  real: [
+    {
+      timestamp: number;
+      production: number;
+      consumption: number;
+      losses: null | number;
+      frequency: number;
+      system_balance: number;
+      ac_balance: number;
+      production_renewable: number;
+      solar_energy_production: null | number;
+    }
+  ];
+  plan: [
+    {
+      timestamp: number;
+      production: number;
+      consumption: number;
+      losses: null | number;
+      frequency: number;
+      system_balance: number;
+      ac_balance: number;
+      production_renewable: number;
+      production_renewable_operator: number;
+      solar_energy_forecast: number;
+      solar_energy_forecast_operator: number;
+    }
+  ];
+}
+
+interface Response {
+  data: Data;
+  success: boolean;
+}
 
 function App() {
+  const [data, setData] = useState<Data>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const now = new Date();
+    const yesterday = new Date(now.getTime() - 24 * 60 * 60 * 1000);
+    const start = yesterday.toISOString();
+    const tomorrow = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+    const end = tomorrow.toISOString();
+
+    setIsLoading(true);
+    fetch(
+      `https://dashboard.elering.ee/api/system/with-plan?start=${start}&end=${end}`
+    )
+      .then((response) => response.json())
+      .then((res: Response) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(true);
+        console.log(err, "Error fetching the api");
+      });
+  }, [data?.toString()]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>There was an error loading the page</div>;
+  }
+
+  const dateFormatter = (date: any) => {
+    // return moment(date).unix();
+
+    return moment(date).format("DD/MM/YY");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="wrapper-container">
+      <h1>Cleanest renewable rates:</h1>
+
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data?.real}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="timestamp" scale="time" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="production_renewable"
+            stroke="#4bc8e2"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
